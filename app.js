27df282957a3fa190e95f85f1358940b91da0ca7
Rohit Gupta <rohitgupta9769@gmail.com>
2022-03-31 03:55:28 +0530

    Procfile added

--- app.js
+++ app.js
@@ -33,9 +33,7 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get("/",(req,res)=>{
-  res.send("Jobs API");
-})
+
 // routes
 app.use("/api/v1/auth",authRouter);
 app.use("/api/v1/jobs", authMiddleware, jobsRouter);
@@ -44,7 +42,7 @@ app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
