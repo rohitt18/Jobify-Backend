72f286150bde33690f3d0cb8081d02a07674ad92
Rohit Gupta <rohitgupta9769@gmail.com>
2022-03-31 02:22:25 +0530

    final changes

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/",(req,res)=>{
+  res.send("Jobs API");
+})
 // routes
 app.use("/api/v1/auth",authRouter);
 app.use("/api/v1/jobs", authMiddleware, jobsRouter);
@@ -45,9 +49,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
